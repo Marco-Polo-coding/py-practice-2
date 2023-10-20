40f4a4280ce1a2e7c4f860a3b54ae723bdc269af
Marco-Polo-coding <dawtardeiestiernogalvan@gmail.com>
2023-10-20 20:04:02 +0200

    Ejercicio1_Practica2

--- ex1.py
+++ ex1.py
@@ -6,11 +6,17 @@ Exercices for input
 name = input("What is your name? ")
 
 # 2. Ask the user for his/her age
+age = input("What is your age? ")
 
 # 3. Ask the user for his/her favorite_color
+favorite_color = input("What's your favorite color ")
 
 # 4. Ask the user for his/her favorite_number
+favorite_number = input("What's your favorite number ")
 
 # 5. Print out the information in a sentence
+print("Hello, ","world!")
 
 # 6. Multiply the user's favorite number by 10 and print out the result
+result = int(favorite_number)*10
+print(result)
